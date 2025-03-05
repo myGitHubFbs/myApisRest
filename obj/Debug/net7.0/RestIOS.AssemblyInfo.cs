9404c08a2689f03e5d60bf8fe57f69818ebb23ec
unknown <fbautista@HR18SIA87.sfa2k18.chih>
2025-03-05 11:13:16 -0600

    Commit con Dockerfile

--- obj/Debug/net7.0/RestIOS.AssemblyInfo.cs
+++ obj/Debug/net7.0/RestIOS.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("2a200a7f-f46e-4e50-94bc-00de71e89927")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("RestIOS")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
